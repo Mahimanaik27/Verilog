0e55668daf45778dd5a9c48721ebcc2dffc3a012
Mahimanaik27 <mahimamnaik2004@gmail.com>
2025-06-01 15:30:45 +0530

    Create design.v

---
+++ assignment1/design.v
@@ -0,0 +1,13 @@
+module mux4to1(a, b, c, d, sel, y);
+  input a, b, c, d;
+  input [1:0] sel;
+  output reg y;
+  always @(*) begin
+    case(sel)
+      2'b00: y = a;
+      2'b10: y = b;
+      2'b11: y = c;
+      2'b01: y = d;
+    endcase
+  end
+endmodule
